57c0947d23bfd63591a923883789e774d119b386
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-20 16:04:37 -0500

    last bit of refactoring

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,31 +6,26 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class StudentTest {
-
-    Student withGrade;
+    Student Tyler;
 
     @Before
     public void setup(){
-        withGrade = new Student("Bill", 456789L);
-        withGrade.addGrade(85);
-        withGrade.addGrade(87);
+        Student Tyler = new Student("Tyler", 123456L);
     }
 
     @Test
     public void checkGetName(){
-        Student Tyler = new Student("Tyler", 123456L);
         assertFalse(Tyler.getName().equals("Bill"));
+        assertEquals("Tyler", Tyler.getName());
     }
 
     @Test
     public void checkGetId(){
-        Student Tyler = new Student("Tyler", 123456L);
         assertNotEquals(234567L, Tyler.getId());
     }
 
     @Test
     public void checkAddGrade(){
-        Student Tyler = new Student("Tyler", 123456L);
         Tyler.addGrade(92);
         Tyler.addGrade(97);
         assertTrue(Tyler.contains(92));
@@ -39,7 +34,6 @@ public class StudentTest {
 
     @Test
     public void checkGradeAverage(){
-        Student Tyler = new Student("Tyler", 123456L);
         Tyler.addGrade(95);
         Tyler.addGrade(97);
         assertEquals(96, Tyler.getGradeAverage());
